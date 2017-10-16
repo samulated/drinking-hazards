0fea173816ec1b11e61dee62844b0b59f964516a
samulated <Telephone1>
2017-10-17 01:47:19 +1100

    Prototype V0.01  - Main Menu Bluebox  - Join Game Bluebox  - Create Game Blank Activity  - Main Game Blank Activity

--- app/src/main/java/com/samuelfawcett/verx/drinkinghazards/MainMenu.java
+++ app/src/main/java/com/samuelfawcett/verx/drinkinghazards/MainMenu.java
@@ -1,7 +1,9 @@
 package com.samuelfawcett.verx.drinkinghazards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void joinGame(View view)
+    {
+        Intent intent = new Intent(this, JoinGame.class);
+
+        startActivity(intent);
+    }
+
+    public void createGame(View view)
+    {
+        Intent intent = new Intent(this, CreateGame.class);
+
+        startActivity(intent);
     }
 }
